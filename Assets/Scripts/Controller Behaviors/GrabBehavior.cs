fcafac5986bb6702530726f692a494e624ae9f77
Jordan Miller <jordanmiller829@gmail.com>
2016-10-17 17:35:25 -0700

    Initial commit

---
+++ Assets/Scripts/Controller Behaviors/GrabBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabBehavior : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device device;
+    private GameObject savedGO;
+    private Transform prevParent;
+
+    void Awake()
+    {
+        // Get reference to controller
+        trackedObj = transform.parent.GetComponent<SteamVR_TrackedObject>();
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        // Grab object
+        if (other.gameObject.tag == "movable" && device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            prevParent = other.transform.parent;
+            savedGO = other.gameObject;
+            savedGO.transform.SetParent(transform);
+        }
+
+        // Release object
+        else if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger) && savedGO != null)
+        {
+            savedGO.transform.SetParent(prevParent);
+            savedGO = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Controller Behaviors/TeleportBehavior.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class TeleportBehavior : MonoBehaviour {
+
+	private GameObject player;
+	public bool isDelayed = false;
+	public bool isOnGround = false;
+
+	void OnCollisionEnter(Collision other) {
+
+		if (!isDelayed) 
+			if (OnTopOf(other))
+				Teleport ();
+	}
+
+	// For "Delayed" teleporter. Make sure the teleporter is on top of an object
+	void OnCollisionStay (Collision other) {
+
+		isOnGround = OnTopOf(other);
+	}
+
+	void OnCollisionExit (Collision other) {
+		
+		isOnGround = false;
+	}
+
+	void Update() {
+
+		// Delete if tele falls below certain threshold. Might want to change to calculate lifetime
+		if (transform.position.y < -30.0f)
+			Destroy (gameObject);
+	}
+
+	public void Teleport () {
+		
+		player = GameObject.Find ("FPSController");
+		player.transform.position = transform.position;
+		Destroy (gameObject);
+	}
+
+	private bool OnTopOf (Collision other) {
+
+		// Make sure we only teleport ontop of an object. Ignore collisions with sides
+		Vector3 normal = other.contacts [0].normal;
+		if (normal.y > 0)
+			return true;
+		else
+			return false;
+	}
+}
